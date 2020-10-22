7153de905ea0f39fdcf812c53018933aa93ba74d
PHÚC NGUYỄN THÀNH VĨNH <1712675@student.hcmus.edu.vn>
2020-10-22 15:11:46 +0700

    Commit SplashScreen, HomeScreen (cont)

--- FoodRecipeApp/GUI/DetailScreen.xaml.cs
+++ FoodRecipeApp/GUI/DetailScreen.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FoodRecipeApp.GUI
@@ -17,7 +18,7 @@ namespace FoodRecipeApp.GUI
 	/// <summary>
 	/// Interaction logic for DetailScreen.xaml
 	/// </summary>
-	public partial class DetailScreen : Window
+	public partial class DetailScreen : Page
 	{
 		public DetailScreen()
 		{
--- FoodRecipeApp/GUI/SearchScreen.xaml.cs
+++ FoodRecipeApp/GUI/SearchScreen.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FoodRecipeApp.GUI
@@ -18,40 +18,11 @@ namespace FoodRecipeApp.GUI
 	/// <summary>
 	/// Interaction logic for SearchScreen.xaml
 	/// </summary>
-	public partial class SearchScreen : MetroWindow
+	public partial class SearchScreen : Page
 	{
 		public SearchScreen()
 		{
 			InitializeComponent();
-		}
-		private void LaunchGitHubSite(object sender, RoutedEventArgs e)
-		{
-			// Launch the GitHub site...
-		}
-
-		private void DeployCupCakes(object sender, RoutedEventArgs e)
-		{
-			// deploy some CupCakes...
-		}
-
-		private MetroWindow _accentThemeTestWindow;
-
-		private void ChangeAppStyleButtonClick(object sender, RoutedEventArgs e)
-		{
-			if (_accentThemeTestWindow != null)
-			{
-				_accentThemeTestWindow.Activate();
-				return;
-			}
-
-			_accentThemeTestWindow = new AccentStyleWindow
-			{
-				Owner = this
-			};
-			_accentThemeTestWindow.Closed += (o, args) => _accentThemeTestWindow = null;
-			_accentThemeTestWindow.Left = this.Left + this.ActualWidth / 2.0;
-			_accentThemeTestWindow.Top = this.Top + this.ActualHeight / 2.0;
-			_accentThemeTestWindow.Show();
 		}
 	}
 }

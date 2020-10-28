876d0a4753e83e557b9873ef3ac58d7ef81498e2
PHÚC NGUYỄN THÀNH VĨNH <1712675@student.hcmus.edu.vn>
2020-10-28 17:35:46 +0700

    Commit Dish, MainPgae

---
+++ FoodRecipeApp_Backup_2020.10.26_12.59.44/ViewModels/NasdaqViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRecipeApp.ViewModels
+{
+	public class NasdaqViewModel : INotifyPropertyChanged
+	{
+		public NasdaqViewModel()
+		{
+			this._displayValue = 3498;
+		}
+
+		private double _displayValue;
+		public double DisplayValue
+		{
+			get => this._displayValue;
+			set
+			{
+				if (this._displayValue != value)
+				{
+					this._displayValue = value;
+					this.OnPropertyChanged("DisplayValue");
+				}
+			}
+		}
+
+		public void UpdateDisplayValue()
+		{
+			if (this.DisplayValue == 3498)
+			{
+				this.DisplayValue = 3470;
+			}
+			else
+			{
+				this.DisplayValue = (int)3498;
+			}
+		}
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}

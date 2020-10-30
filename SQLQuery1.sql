c3c7b9218809005635774c94b93a369d33c44f6e
VÕ THIỆN TÍN <vothientin@gmail.com>
2020-10-30 17:57:48 +0700

    Search with checkBox, refresh search page

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1827,4 +1827,44 @@ where not exists ((select Item from dbo.SplitInts(@List,','))
 								except (select Item from dbo.SplitInts(Loai,',')))
 END
 GO
-EXEC USP_getDishByTypes @List = N'Chay'
+-----------------------
+CREATE FUNCTION [dbo].[ufn_removeMark] (@text nvarchar(max))
+RETURNS nvarchar(max)
+AS
+BEGIN
+	SET @text = LOWER(@text)
+	DECLARE @textLen int = LEN(@text)
+	IF @textLen > 0
+	BEGIN
+		DECLARE @index int = 1
+		DECLARE @lPos int
+		DECLARE @SIGN_CHARS nvarchar(100) = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệếìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵýđð'
+		DECLARE @UNSIGN_CHARS varchar(100) = 'aadeoouaaaaaaaaaaaaaaaeeeeeeeeeeiiiiiooooooooooooooouuuuuuuuuuyyyyydd'
+
+		WHILE @index <= @textLen
+		BEGIN
+			SET @lPos = CHARINDEX(SUBSTRING(@text,@index,1),@SIGN_CHARS)
+			IF @lPos > 0
+			BEGIN
+				SET @text = STUFF(@text,@index,1,SUBSTRING(@UNSIGN_CHARS,@lPos,1))
+			END
+			SET @index = @index + 1
+		END
+	END
+	RETURN @text
+END
+go
+
+DROP PROCEDURE USP_getDishByName
+GO
+CREATE PROCEDURE USP_getDishByName
+  @Name NVARCHAR(MAX)
+AS
+BEGIN
+  SET NOCOUNT ON;  
+  select * from DISH
+where dbo.ufn_removeMark(Name) LIKE '%'+ @Name +'%' OR Name LIKE '%'+ @Name +'%'
+END
+GO
+
+EXEC USP_getDishByName @Name = N'sup'
